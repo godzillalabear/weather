66e36091fde21efcf22d540b86245433fd827bee
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-06-30 21:45:48 +0800

    make weather data by seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+l = Location.create(name: "Taipei")
+l.recordings.create(temp: 32, status: "cloudy")
+l.recordings.create(temp: 36, status: "rainy")
+l.recordings.create(temp: 38, status: "rainy")
+l.recordings.create(temp: 33, status: "cloudy")
+l.recordings.create(temp: 35, status: "sunny")
